c2d7a78a63b9955a387d453a9dae28017d22dcdc
Julissa Jansen <julissa@Julissas-MacBook-Air.local>
2015-03-03 21:06:15 -0500

    Added new, show, and simpleform for zine

--- app/controllers/zines_controller.rb
+++ app/controllers/zines_controller.rb
@@ -8,5 +8,18 @@ class ZinesController < ApplicationController
     @zines = Zine.all
   end
 
+  def show
+    @zine = Zine.find(params[:id])
+  end
+
+  def create
+    @zine = Zine.new(manufacturer_params)
+    if @zine.save
+      redirect_to new_manufacturer_path, notice: 'Zine was successfully added'
+    else
+      render :new
+    end
+  end
+
 
 end
